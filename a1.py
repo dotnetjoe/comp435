3a940903c7c0840761a3e2f73c6fc783fdc5c12c
ronaldogdm <ronaldogarciadem@gmail.com>
2019-09-12 13:25:55 -0400

    tryna update

---
+++ a1.py
@@ -0,0 +1,23 @@
+import fileinput
+
+hexCount = {}
+count = 1
+
+for line in fileinput.input():
+    line = line.strip()
+    line = int(line, 16)
+
+    if line not in hexCount:
+        # If the line isn't a duplicate, initialize the number of times it has appeared
+        hexCount[line] = 1
+    else:
+        # If the line is a duplicate, increment the amount of times it appears
+        hexCount[line] += 1
+
+intValues = list(hexCount.keys())
+intValues.sort()
+
+for intValue in intValues:
+    if hexCount[intValue] > 1:
+        # print hex followed by the number of times it appears
+        print("%x %d" % (intValue, hexCount[intValue]))
