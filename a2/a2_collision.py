3dfb66255d1ddb57cca464cc49194ecd3757590c
ronaldogdm <ronaldogarciadem@gmail.com>
2019-09-25 19:45:17 -0400

    “collision”

--- a2/a2_collision.py
+++ a2/a2_collision.py
@@ -9,16 +9,15 @@ hashes = set()
 def RSG():
     return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(6, 100))
 
-def collision():
+
+def find_collision(n):
     hashcounter = 0
     while True:
         generated_string = RSG()
-        generated_hash = hashlib.sha256(generated_string.encode()).hexdigest()[:6]
+        generated_hash = hashlib.sha256(generated_string.encode()).digest()[:n]
         hashcounter += 1
         if generated_hash in hashes:
-            print(plaintext[generated_hash] + " " + generated_string)
-            print(hashcounter)
-            return False
-        else:
+            return plaintext[generated_hash],generated_string
+        else:             
             hashes.add(generated_hash)
             plaintext[generated_hash] = generated_string
